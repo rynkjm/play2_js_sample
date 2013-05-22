118ecab4dd4d7116c1774cb4350a655a844da69e
rynkjm <rynkjm@gmail.com>
2013-05-22 23:16:51 +0900

    fix #4

--- public/javascripts/addressbook.js
+++ public/javascripts/addressbook.js
@@ -1,3 +1,23 @@
 $(function(){
-  // ここに実装
-}
+  // Backbornモデルを継承したAddressクラスを定義
+  var Address = Backbone.Model.extends({
+    // デフォルト値
+    defaults: {
+        name: ''
+    },
+    // 初期化
+    initialize: function(){
+        if (!this.get('name')){
+            this.get((name: this.defaults.name));
+        }
+    },
+    // バリデーション
+    validate: function (attributes){
+        var name = attributes.name;
+        if( !name || name === this.defaults.name){
+            return 'Error!';
+        }
+    }
+  });
+})
+
