751a3744f0076f43f2914d1781af119e622f53f9
rynkjm <rynkjm@gmail.com>
2013-05-24 23:42:20 +0900

    fix #6

--- public/javascripts/addressbook.js
+++ public/javascripts/addressbook.js
@@ -27,6 +27,34 @@ $(function(){
     // addressbook-sampleというキーでlocalStorageを扱う
     localStorage: new Store('addressbook-sample')
   });
+  
+  // Backborn.Viewを継承してAddressを表示する
+  // AddressViewクラスを定義
+  var AddressView = Backbone.View.extends({
+    tagName: 'li', 
+    className: 'address-item',
+    // イベントハンドラの設定
+    events: {
+      'dbclick label.name': 'rename' ,
+      'click button.delete': 'clear'
+    },
+    initialize: function(){
+      this.model.bind('change', this.render, this);
+      this.model.bind('destroy', this.remove, this);
+    },
+    render: function(){
+      $(this.el).html(
+        $('<label class="name">').text(this.model.get('name'))
+      ).append('<button class="delete">Delete</button>');
+      return this;
+    },
+    rename: function(){
+      var newName = window.prompt('Enter new name.', this.model.get('name'));
+    },
+    clear: function(){
+      this.model.destroy();
+    }
+  });
 
   // インスタンスの生成
   var Addresses = new AddressCollection;
